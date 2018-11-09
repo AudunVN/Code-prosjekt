d43b7b0960939d9285d6511999e2a2f93525260a
Space <space@471.no>
2018-11-09 13:31:12 +0100

    Added update and remove buttons to cart

--- js/cart.js
+++ js/cart.js
@@ -2,30 +2,3 @@ function addToCart(item, count) {
     console.log("Adding " + count + " of " + item.name + " (ID: " + item.id + ") to cart");
     setCartItemCount(item.id, getCartItemCount(item.id) + parseInt(count));
 }
-
-function setCartItemCount(itemId, count) {
-	var item = getItemData(itemId, products);
-	if (item) {
-		if (count > 0) {
-            var existingItem = cart.filter(e => e.id === itemId);
-            if (existingItem.length != 0) {
-                /* product exists, set count */
-                existingItem[0].count = count;
-            } else {
-                var newItem = {
-                    id: itemId,
-                    count: count
-                };
-                cart.push(newItem);
-            }
-            storeCart();
-		} else {
-            var existingItem = cart.filter(e => e.id === itemId);
-            if (existingItem.length != 0) {
-                /* remove item if it exists */
-                cart.splice(cart.indexOf(existingItem), 1);
-            }
-        }
-        updateCartCounter();
-	}
-}
